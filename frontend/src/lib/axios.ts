ab3f57df2b232a6ab0538edddda7ca102f75d6cb
osama <osamasaif242@gmail.com>
2025-07-02 03:43:17 +0300

    Vite_Backend_Url

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,11 @@
 import { useAuthStore } from "@/store/useAuthStore";
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5000/api' : '/api';
+const BASE_URL = 
+import.meta.env.MODE === 'development' ? 
+'http://localhost:5000/api' :
+import.meta.env.VITE_BACKEND_URL + '/api'
+;
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
