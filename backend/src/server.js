c1532b5e19cec834bf3eb10a31c16c87ab3cb9e7
osama <osamasaif242@gmail.com>
2025-07-02 03:50:09 +0300

    Update Cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,7 +32,7 @@ connectDb();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: [process.env.FRONTEND_URL],
     credentials: true
 }));
 
